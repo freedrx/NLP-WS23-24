d965628a4fca41252077714375ffa4a9dafade3e
kilianhuemmer <kilian.huemmer@study.thws.de>
2024-01-14 21:50:52 +0100

    filtered content

--- chatbot/datacleaner.py
+++ chatbot/datacleaner.py
@@ -1,7 +1,7 @@
-import re
 import pandas as pd
-import time
 import emoji
+
+
 
 """
 Datacollection Process:
@@ -10,6 +10,7 @@ We gathered the dad jokes data from kaggle dataset: https://www.kaggle.com/datas
 
 We used this code to clean the data and remove any jokes that were not suitable for our model. We i.e. removed any emojis and contractions from the dataset or removed jokes that were too long or too short.
 
+We filtered the code for any jokes that contained offensive words or phrases.
 """
 
 
@@ -85,7 +86,89 @@ class DadJokesCleaner:
         }
         df['joke'] = df['joke'].replace(contractions_dict, regex=True) # replace contractions
 
-        df.to_csv('cleaned_dataset1.csv', index=False) # save cleaned dataset
+
+        # remove words that are not suitable for our model
+        filter_dict = {
+            "sex", "dick", "cock", "penis", "pussy", "nazi", "hitler", 
+            "homosexual", "homo", "vagina", "fuck", "shit", "ass", 
+            "bastard", "bitch", "cunt", "whore", "slut", "fag", 
+            "dyke", "retard", "idiot", "stupid", "moron", "douchebag",
+            "n-word", "kike", "spic", "chink", "gook", "wop", "gay", "nigga",
+            "cracker", "redneck", "hillbilly", "white trash", 
+            "terrorist", "jihad", "isis", "kkk", "murderer", "rapist",
+            "abortion", "al-Qaeda", "anal", "anarchy", "anus", "arson", 
+            "asshole", "bastard", "bigot", "blasphemy", "bolshevik", 
+            "bong", "booze", "cocaine", "commie", "crack", "damn", 
+            "date rape", "devil-worship", "dildo", "drugs", "ecstasy", 
+            "fascist", "fatso", "felching", "fellatio", "fetish", 
+            "fornicate", "gangbang", "genital", "god damn", "goddamn", 
+            "guro", "heroin", "homicide", "incest", "jackass", "jerk", "weapon",
+            "jigaboo", "kinky", "klan", "lesbian", "masturbate", 
+            "meth", "molest", "muff", "murder", "naked", "necrophilia", 
+            "nipple", "opiate", "opium", "orgasm", "pedophile", "piss", 
+            "poop", "prostitute", "pubic", "queer", "racist", 
+            "rape", "rectum", "satanic", "scat", "semen", "sexism", 
+            "sexual", "shroom", "snorting", "sodomize", "sodomy", 
+            "spastic", "suicide", "terror", "threesome", "toke", 
+            "torture", "toxic", "transexual", "underage", "urinate", 
+            "urine", "vomit", "voyeur", "weed", "white power", 
+            "wigger", "witch", "xxx", "exploit", "hate", "violence", "bully", "abuse", 
+            "harass", "threat", "illegal", "fraud", "scam", 
+            "phishing", "misogyny", "misandry", "bigamy", 
+            "cult", "extremist", "radical", "supremacist", 
+            "xenophobe", "hazard", "injury", "risk", "unsafe", 
+            "anorexia", "bulimia", "self-harm", "self-hate", 
+            "addict", "addiction", "gamble", "gambling", 
+            "weapons", "terrorism", "exploitation", "cult", 
+            "abduct", "kidnap", "suicidal", "bomb", "gun",
+            "anarchist", "bloodshed", "exploit", "intolerance",
+            "kidnapping", "cultist", "sabotage", "sedition",
+            "slander", "stalking", "terrorism", "torture", "violator","bastard", "bitch", "cunt", "whore", "slut", "fag", 
+            "dyke", "retard", "idiot", "stupid", "moron", "douchebag",
+            "n-word", "kike", "spic", "chink", "gook", "wop", 
+            "cracker", "redneck", "hillbilly", "white trash", 
+            "terrorist", "jihad", "isis", "kkk", "murderer", "rapist",
+            "abortion", "al-Qaeda", "anal", "anarchy", "anus", "arson", 
+            "asshole", "bastard", "bigot", "blasphemy", "bolshevik", 
+            "bong", "booze", "cocaine", "commie", "crack", "damn", 
+            "date rape", "devil-worship", "dildo", "drugs", "ecstasy", 
+            "fascist", "fatso", "felching", "fellatio", "fetish", 
+            "fornicate", "gangbang", "genital", "god damn", "goddamn", 
+            "guro", "heroin", "homicide", "incest", "jackass", "jerk", 
+            "jigaboo", "kinky", "klan", "lesbian", "masturbate", 
+            "meth", "molest", "muff", "murder", "naked", "necrophilia", 
+            "nipple", "opiate", "opium", "orgasm", "pedophile", "piss", 
+            "poop", "prostitute", "pubic", "queer", "racist", 
+            "rape", "rectum", "satanic", "scat", "semen", "sexism", 
+            "sexual", "shroom", "snorting", "sodomize", "sodomy", 
+            "spastic", "suicide", "terror", "threesome", "toke", 
+            "torture", "toxic", "transexual", "underage", "urinate", 
+            "urine", "vomit", "voyeur", "weed", "white power", 
+            "wigger", "witch", "xxx", "exploit", "hate", "violence", "bully", "abuse", 
+            "harass", "threat", "illegal", "fraud", "scam", 
+            "phishing", "misogyny", "misandry", "bigamy", 
+            "cult", "extremist", "radical", "supremacist", 
+            "xenophobe", "hazard", "injury", "risk", "unsafe", 
+            "anorexia", "bulimia", "self-harm", "self-hate", 
+            "addict", "addiction", "gamble", "gambling", 
+            "weapons", "terrorism", "exploitation", "cult", 
+            "abduct", "kidnap", "suicidal", "bomb", "gun",
+            "anarchist", "bloodshed", "exploit", "intolerance",
+            "kidnapping", "cultist", "sabotage", "sedition",
+            "slander", "stalking", "terrorism", "torture", "violator", "niga", "niger", "nigger"
+        }
+
+        # Create a regular expression pattern for filtering
+        pattern = '|'.join(rf'\b{word}\b' for word in filter_dict)
+        pattern2 = '|'.join(rf'{word}' for word in filter_dict)
+
+        # Use boolean indexing to filter out rows containing offensive content
+        df = df[~df['joke'].str.contains(pattern, case=False, regex=True)]
+        df = df[~df['joke'].str.contains(pattern2, case=False, regex=True)]    
+
+                  
+
+        df.to_csv('cleaned_dataset.csv', index=False) # save cleaned dataset
 
 
 #Execute the code
